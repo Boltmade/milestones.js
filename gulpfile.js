e9a75280a504ada72c2899295b02fb652a118460
James Blashill <james@blashill.com>
2016-05-13 14:51:35 -0400

    Ditch webpack and do it the ol' fashioned way

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,12 @@
 'use strict';
 
 const gulp = require('gulp');
-const webpack = require('webpack-stream');
 const babel = require('gulp-babel');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 
 gulp.task('default', function() {
   return gulp.src('milestones.js')
-    .pipe(webpack())
     .pipe(babel({ presets: ['es2015'] }))
     .pipe(uglify())
     .pipe(concat('milestones.js'))
--- milestones.js
+++ milestones.js
@@ -1,47 +1,56 @@
 'use strict';
 
-class Milestone {
-  constructor(topic) {
-    this.topic = topic;
-    this.promise = new Promise((resolve, reject) => {
-      this.resolve = resolve;
-      this.reject = reject;
-    });
-  }
+(function(context) {
 
-  link(externalPromise) {
-    if (this.externalPromise) throw new Error('Milestone is already linked to a promise');
-
-    this.externalPromise = externalPromise;
-    this.externalPromise.then(this.resolve).catch(this.reject);
-    return this;
-  }
-
-  reached() {
-    return this.promise;
-  }
-}
-
-class Milestones {
-  constructor() {
-    this.registry = {};
-  }
-
-  register(topic, promise) {
-    return this.getOrCreate(topic).link(promise);
-  }
-
-  getOrCreate(topic) {
-    if (!this.registry[topic]) {
-      this.registry[topic] = new Milestone(topic);
+  class Milestone {
+    constructor(topic) {
+      this.topic = topic;
+      this.promise = new Promise((resolve, reject) => {
+        this.resolve = resolve;
+        this.reject = reject;
+      });
     }
-    return this.registry[topic];
+
+    link(externalPromise) {
+      if (this.externalPromise) throw new Error('Milestone is already linked to a promise');
+
+      this.externalPromise = externalPromise;
+      this.externalPromise.then(this.resolve).catch(this.reject);
+      return this;
+    }
+
+    reached() {
+      return this.promise;
+    }
   }
 
-  when() {
-    let topics = Array.prototype.slice.call(arguments, 0);
-    return Promise.all(topics.map((topic) => this.getOrCreate(topic).reached()))
-  }
-}
+  class Milestones {
+    constructor() {
+      this.registry = {};
+    }
 
-module.exports = Milestones;
+    register(topic, promise) {
+      return this.getOrCreate(topic).link(promise);
+    }
+
+    getOrCreate(topic) {
+      if (!this.registry[topic]) {
+        this.registry[topic] = new Milestone(topic);
+      }
+      return this.registry[topic];
+    }
+
+    when() {
+      let topics = Array.prototype.slice.call(arguments, 0);
+      return Promise.all(topics.map((topic) => this.getOrCreate(topic).reached()))
+    }
+  }
+
+  if (typeof(window) !== 'undefined') {
+    window.Milestones = Milestones;
+  } else {
+    module.exports = Milestones;
+  }
+
+})();
+
